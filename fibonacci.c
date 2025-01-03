b903fc0f7f5ba0623abaaf7a0747aed8e32a59e8
Prasanna Alagesan <prasanna.alagesan@solitontech.local>
2025-01-03 18:43:57 +0530

    Added feature task 4

--- fibonacci.c
+++ fibonacci.c
@@ -69,6 +69,11 @@ int sumSeries(int *series, int n){
     return bugSum;
 }
 
+int nthelement(int* series){
+    int n = sizeof(series)/sizeof(series[0]);
+    return series[n-1];
+}
+
 int main(){
     int n;
     int bugSum;
@@ -97,4 +102,5 @@ int main(){
     bugSum = sumSeries(terms, n);
     printf("Bugsum of the series - %d", bugSum);
     
+    printf("nth element is %d", nthelement(terms) );
 }
