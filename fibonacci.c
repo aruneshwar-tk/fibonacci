09a7cc7f9d085b7d0e595c62289444d37479d513
Kabil Murugan <kabil.murugan@solitontech.local>
2025-01-03 16:58:29 +0530

    Add bugSumSeries function to return bugSum

--- fibonacci.c
+++ fibonacci.c
@@ -12,7 +12,6 @@ int* generateFibonacciSeries(int n){
     int t1 = 0, t2 = 1;
     int* series = (int*)malloc(n * sizeof(int));
 
-
     int nextTerm = t1 + t2;
     series[0] = t1;
     series[1] = t2;
@@ -26,12 +25,33 @@ int* generateFibonacciSeries(int n){
     return series;
 }
 
+int bugSumSeries(int *series, int n){
+    /*A function to return the bug sum of the 
+    series - always 1 less than the expected sum*/
+
+    //args = series (array containing the terms)
+    //n = size of the series
+
+    int bugSum = 0;
+
+    for (int i=2;i<n;i++){
+        bugSum += series[i];
+    }
+
+    return bugSum;
+}
+
 int main(){
     int n;
+    int bugSum;
     printf("Enter the no. of terms: ");
     scanf("%d", &n);
     int *terms = generateFibonacciSeries(n);
     for (int i=0;i<n;i++){
         printf("%d\n", terms[i]);
     }
+
+    bugSum = bugSumSeries(terms, n);
+    printf("Bugsum of the series - %d", bugSum);
+    
 }
